31a862c0e376adb46d62b254ef954a65dfbce454
Pradip Asodariya <asodariyapradip@gmail.com>
2023-02-04 11:06:10 +0900

    create project structure
    Defines the project structure and create the required components and service.

--- chat-app/src/app/app.module.ts
+++ chat-app/src/app/app.module.ts
@@ -4,10 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { GraphQLModule } from './graphql.module';
 import { HttpClientModule } from '@angular/common/http';
+import { ChatScreenComponent } from './components/chat-screen/chat-screen.component';
+import { MessagePanelComponent } from './components/message-panel/message-panel.component';
+import { UserPanelComponent } from './components/user-panel/user-panel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChatScreenComponent,
+    MessagePanelComponent,
+    UserPanelComponent
   ],
   imports: [
     BrowserModule,
